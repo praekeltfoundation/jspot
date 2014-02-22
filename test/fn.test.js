83de1ac839c4bae5e3ec3dc84d2bb6169b36abc8
justinvdm <justinvdm.0.1.0@gmail.com>
2014-02-22 13:34:11 +0200

    Basic 'recording' translation function to record the extracted gettext calls

---
+++ test/fn.test.js
@@ -0,0 +1,29 @@
+var assert = require('assert');
+var fn = require('../lib/fn');
+
+describe("fn", function() {
+    it("should proxy to .gettext when called directly", function() {
+        assert.deepEqual(fn('foo'), {
+            method: 'gettext',
+            params: {message: 'foo'}
+        });
+    });
+
+    describe(".gettext", function() {
+        it("should throw an error if the message param is unusable",
+        function() {
+            assert.throws(
+                function() { fn(null); },
+                new RegExp([
+                    "gettext was given a value of type 'object'",
+                    "instead of a string or number: null"].join(' ')));
+        });
+
+        it("should return the method call's data", function() {
+            assert.deepEqual(fn('foo'), {
+                method: 'gettext',
+                params: {message: 'foo'}
+            });
+        });
+    });
+});
