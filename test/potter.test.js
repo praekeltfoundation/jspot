e7c75bb7c5ca98a12ce5a8151feb2c2a405cb95f
justinvdm <justinvdm.0.1.0@gmail.com>
2014-02-23 20:07:47 +0200

    Basic conversion from jspot's extraction formation to the pot format accepted by gettext-parser

--- test/potter.test.js
+++ test/potter.test.js
@@ -1,9 +1,195 @@
+var assert = require('assert');
+var potter = require('../lib/potter');
+
+
 describe("potter", function() {
-    it("should create a new domain pot if none currently exists");
+    it("should create a new domain pot if none currently exists", function() {
+        var pots = {error: {}};
 
-    it("should create a new context if none currently exists");
+        potter([{
+            key: 'foo',
+            plural: null,
+            context: '',
+            domain: 'messages',
+            line: 1,
+            filename: 'ham.js'
+        }, {
+            key: 'bar',
+            plural: null,
+            context: '',
+            domain: 'lerps',
+            line: 2,
+            filename: 'spam.js'
+        }], pots);
 
-    it("should create a new translation if none currently exists");
+        assert('translations' in pots.messages);
+        assert.equal(pots.messages.charset, 'utf-8');
+        assert.deepEqual(pots.messages.headers, {
+            'project-id-version': 'PACKAGE VERSION',
+            'language-team': 'LANGUAGE <LL@li.org>',
+            'po-revision-date': 'YEAR-MO-DA HO:MI+ZONE',
+            'language': '',
+            'mime-version': '1.0',
+            'content-transfer-encoding': '8bit'
+        });
 
-    it("should append to a translation if one already exists");
+        assert('translations' in pots.lerps);
+        assert.equal(pots.lerps.charset, 'utf-8');
+        assert.deepEqual(pots.lerps.headers, {
+            'project-id-version': 'PACKAGE VERSION',
+            'language-team': 'LANGUAGE <LL@li.org>',
+            'po-revision-date': 'YEAR-MO-DA HO:MI+ZONE',
+            'language': '',
+            'mime-version': '1.0',
+            'content-transfer-encoding': '8bit'
+        });
+    });
+
+    it("should create a new context if none currently exists", function() {
+        var pots = potter([{
+            key: 'foo',
+            plural: null,
+            context: '',
+            domain: 'messages',
+            line: 1,
+            filename: 'ham.js'
+        }, {
+            key: 'bar',
+            plural: null,
+            context: 'lark',
+            domain: 'lerps',
+            line: 2,
+            filename: 'spam.js'
+        }]);
+
+        assert('' in pots.messages.translations);
+        assert('lark' in pots.lerps.translations);
+    });
+
+    it("should create a new translation if none currently exists", function() {
+        var pots = potter([{
+            key: 'foo',
+            plural: null,
+            context: '',
+            domain: 'messages',
+            line: 1,
+            filename: 'ham.js'
+        }, {
+            key: 'bar',
+            plural: 'bars',
+            context: 'lark',
+            domain: 'lerps',
+            line: 2,
+            filename: 'spam.js'
+        }]);
+
+        assert.deepEqual(pots.messages.translations[''].foo, {
+            msgid: 'foo',
+            msgstr: [''],
+            msgctx: '',
+            comments: {reference: 'ham.js:1'}
+        });
+
+        assert.deepEqual(pots.lerps.translations.lark.bar, {
+            msgid: 'bar',
+            msgid_plural: 'bars',
+            msgstr: ['', ''],
+            msgctx: 'lark',
+            comments: {reference: 'spam.js:2'}
+        });
+    });
+
+    it("should append to a translation if one already exists", function() {
+        var pots = {
+            messages: {
+                translations: {
+                    '': {
+                        foo: {
+                            msgid: 'foo',
+                            msgstr: [''],
+                            msgctx: '',
+                            comments: {reference: 'lamb.js:3'}
+                        }
+                    },
+                }
+            },
+            lerps: {
+                translations: {
+                    lark: {
+                        bar: {
+                            msgid: 'bar',
+                            msgid_plural: 'bars',
+                            msgstr: ['', ''],
+                            msgctx: 'lark',
+                            comments: {reference: 'ram.js:4'}
+                        }
+                    }
+                }
+            }
+        };
+
+        potter([{
+            key: 'foo',
+            plural: null,
+            context: '',
+            domain: 'messages',
+            line: 1,
+            filename: 'ham.js'
+        }, {
+            key: 'bar',
+            plural: 'bars',
+            context: 'lark',
+            domain: 'lerps',
+            line: 2,
+            filename: 'spam.js'
+        }], pots);
+
+        assert.deepEqual(pots.messages.translations[''].foo, {
+            msgid: 'foo',
+            msgstr: [''],
+            msgctx: '',
+            comments: {reference: 'lamb.js:3 ham.js:1'}
+        });
+
+        assert.deepEqual(pots.lerps.translations.lark.bar, {
+            msgid: 'bar',
+            msgid_plural: 'bars',
+            msgstr: ['', ''],
+            msgctx: 'lark',
+            comments: {reference: 'ram.js:4 spam.js:2'}
+        });
+    });
+
+    it("should not append to a translation if its reference exists", function() {
+        var pots = {
+            messages: {
+                translations: {
+                    '': {
+                        foo: {
+                            msgid: 'foo',
+                            msgstr: [''],
+                            msgctx: '',
+                            comments: {reference: 'lamb.js:3 ham.js:1'}
+                        }
+                    },
+                }
+            }
+        };
+
+        potter([{
+            key: 'foo',
+            plural: null,
+            context: '',
+            domain: 'messages',
+            line: 1,
+            filename: 'ham.js'
+        }], pots);
+
+        assert.deepEqual(pots.messages.translations[''].foo, {
+            msgid: 'foo',
+            msgstr: [''],
+            msgctx: '',
+            comments: {reference: 'lamb.js:3 ham.js:1'}
+        });
+    });
 });
