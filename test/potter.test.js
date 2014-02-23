e5fc993ca9826462030953b21105eaeb15359c0e
justinvdm <justinvdm.0.1.0@gmail.com>
2014-02-23 19:34:17 +0200

    Test stubs for the potter module

---
+++ test/potter.test.js
@@ -0,0 +1,9 @@
+describe("potter", function() {
+    it("should create a new domain pot if none currently exists");
+
+    it("should create a new context if none currently exists");
+
+    it("should create a new translation if none currently exists");
+
+    it("should append to a translation if one already exists");
+});
