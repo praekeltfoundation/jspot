c48d0ee49c60e8ef3153658f384fbd6ff7b26fe3
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-08 23:23:00 +0200

    Test stubs for jspot.extractors

---
+++ test/extractors/extractors.test.js
@@ -0,0 +1,17 @@
+describe("jspot.extractors", function() {
+    describe(".support", function() {
+        describe("the returned function", function() {
+            it("should apply defaults before invoking the extractor");
+        });
+
+        it("should add the extractor to the registry");
+    });
+
+    describe(".get", function() {
+        it("should get an extractor using the given extension");
+    });
+
+    describe(".get.by_filename", function() {
+        it("should get an extractor using the given filename");
+    });
+});
