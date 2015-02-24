ab4ded5b788d66f4f7cac58a21aa1e237cea7340
justinvdm <justinvdm.0.1.0@gmail.com>
2015-02-24 10:26:58 +0200

    Allow external extractors to be plugged in

---
+++ test/fixtures/extract/extractors-override/input/jspot-js.js
@@ -0,0 +1,15 @@
+var _extract = require('lib/extractors/js');
+
+
+function extract(opts) {
+    var results = _extract(opts);
+
+    results.forEach(function(d) {
+        d.key += '!';
+    });
+
+    return results;
+}
+
+
+module.exports = extract;
