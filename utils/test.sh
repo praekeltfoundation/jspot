201ef9f3e24c656af6c42240d1c83043b6802aec
Jérôme Steunou <jerome.steunou@klaxoon.com>
2018-08-28 10:24:44 +0200

    Add catchExtractError extract options

--- utils/test.sh
+++ utils/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-./node_modules/.bin/jshint `find lib/ test/ -name "*.js"`
+./node_modules/.bin/jshint `find lib/ test/ -not -path "test/fixtures/extract/wrong/*" -name "*.js"`
 ./node_modules/.bin/mocha `find "$@" -name "*.test.js"`
